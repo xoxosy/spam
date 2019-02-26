6437f7e1cf4d9fe02d37597d0f63679100b8878c
mustafafando <47396050+mustafafando@users.noreply.github.com>
2019-02-26 22:26:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("550033741670645772")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`i love probot`);
 }, 30)
 })
 
