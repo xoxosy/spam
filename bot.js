77073da0088083efe9f28291c9239a9c23ca7cc1
mustafafando <47396050+mustafafando@users.noreply.github.com>
2019-02-27 19:57:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,57 +75,6 @@ if (!rank) return message.reply(' ')
   }
 });
 
-client2.on('message', message => {
-    if(message.content === prefix+'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send('#rep <@286088294234718209>')
-    }
-});
-
-client2.on('message', message => {
-if (message.content === prefix+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
 
 
-
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
-
-client.login(process.env.TOKEN);
-client2.login(process.env.TOKEN2);
+client.login(process.env.BOT_TOKEN);
