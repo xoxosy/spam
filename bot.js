73e6d65af47cce63e3756593a9b1a2307ebe00b7
mustafafando <47396050+mustafafando@users.noreply.github.com>
2019-03-14 21:34:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,83 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
+const discord = require("discord.js");
+const randomWord = require("random-words")
 
+const TOKEN = "token"; 
+ 
+const bot = new discord.Client();
 
-client.on("ready", () => {
-let channel =     client.channels.get("550033741670645772")
-setInterval(function() {
-channel.send(`ilove spam`);
-}, 30)
+var spam;
+
+var phrases = ["thing1","another one","keep adding more","there's no limit"];
+
+function startspam()
+{
+    console.log("Spam starting!")
+    var server = bot.guilds.get("548872104813264907");
+    var chan = new discord.TextChannel(server,{"id":"548872104813264909"});
+    spam = bot.setInterval(()=>
+    {
+        chan.send(randomWord()).then(msg=>{ // Sticking with randomwords.
+            console.log(msg.content);
+        });
+ 
+    },1000);
+}
+
+function stopspam()
+{
+    bot.clearTimeout(spam);
+    console.log("Spam Stopped.")
+}
+
+bot.on("ready",()=>{
+    console.log("Ready!");
 })
+
+bot.on("message",msg=>{
+    if(msg.author.id == "416567959067033601")
+    {
+        if(msg.content.toLowerCase() == "=start")
+        {
+            startspam()
+        }
+        else if(msg.content.toLowerCase() == "=stop")
+        {
+            stopspam()
+        }
+    }
+})
+
+bot.on('message', message => {
+    var prefix = "=";
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.id !== "416567959067033601") return;
+  
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+  
+  
+  let args = message.content.split(" ").slice(1);
+  let x = args.join(" ")
+    if(message.content.startsWith(prefix + 'trans')) {
+        message.channel.send(''+x);
+            message.delete(999)
+    }
+    
+   
+  });
+
+bot.on('message', message => {
+    if(message.content.startsWith('daily')) {
+        message.channel.send('#daily');
+  }
+});
+
+bot.on('message', message => {
+    if(message.content.startsWith('credits')) {
+        message.channel.send('#credits');
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
